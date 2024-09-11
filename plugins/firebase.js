9284599aa1e606c6b64448852450cc8981a1b7ac
daurora-net <daurora.net@gmail.com>
2024-09-11 18:58:15 +0900

    first commit

---
+++ plugins/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDvl00BtcPGR3PVXIJC21cQuI4pnyzW6L4",
+    authDomain: "hdd-rental2.firebaseapp.com",
+    projectId: "hdd-rental2",
+    storageBucket: "hdd-rental2.appspot.com",
+    messagingSenderId: "493843761254",
+    appId: "1:493843761254:web:29b1d1d091c4e0e55605a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
